e46e98407fb95d724c62b1337a1b144df4d25fa7
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-02-01 15:43:42 -0700

    Update main.js

--- Basic-JavaScript-Projects/Project1_expressions_alert/js/main.js
+++ Basic-JavaScript-Projects/Project1_expressions_alert/js/main.js
@@ -1,4 +1,4 @@
-window.alert('What\'s up world!');
+window.alert('What\'s up world!'); //use an escaping backslash
 
 document.write("What's up World!");
 
@@ -10,8 +10,17 @@ var bravo = "This string is secondary";
 
 window.alert(bravo);
 
-document.write(alpha+bravo);
+document.write(alpha.fontcolor("red")+bravo.fontcolor("blue"));//add bravo to alpha and differentiate by colors red and blue
 
-var mom="rose", dad="dj", sis1="brit", sis2="mis";
+var mom="rose", dad="dj", sis1="brit", sis2="mis"; //assign multiple variables
 
 document.write(sis1);
+
+document.write(12/3);/*this is the 
+
+best way to write 
+
+multiple lines of
+
+comments for your code*/
+

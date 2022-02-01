f0f37da19f0043e0c75ef0141372ecffe92293d8
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-02-01 16:10:03 -0700

    Create main.js
    Testing functions

---
+++ Basic-JavaScript-Projects/Project2_functions/js/main.js
@@ -0,0 +1,5 @@
+function newFunction(){
+    var part1 = "Firstly, I am serious. ", part2 = "Secondly, don't call my Shirley", voiceLine= document.getElementById("line");
+    voiceLine = part1+part2;
+    return document.write(voiceLine);
+}

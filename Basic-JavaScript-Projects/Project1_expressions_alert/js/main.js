3f2404b3252c2f6199489c8a8d29db1897257e36
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-02-01 15:07:31 -0700

    Create main.js
    Created this file to test some JavaScript

---
+++ Basic-JavaScript-Projects/Project1_expressions_alert/js/main.js
@@ -0,0 +1,17 @@
+window.alert('What\'s up world!');
+
+document.write("What's up World!");
+
+var alpha = "This is the first string";
+
+document.write(alpha);
+
+var bravo = "This string is secondary";
+
+window.alert(bravo);
+
+document.write(alpha+bravo);
+
+var mom="rose", dad="dj", sis1="brit", sis2="mis";
+
+document.write(sis1);

c926478818ad80c1f9d1a12d57a4f32833653c63
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-02-04 10:13:32 -0700

    Update main.js
    Added the game function.

--- Basic-JavaScript-Projects/Project7_scope_time_function/js/main.js
+++ Basic-JavaScript-Projects/Project7_scope_time_function/js/main.js
@@ -1,28 +1,42 @@
 var x = 3;
 
-function newFunction(){
+function newFunction() {
     var y = 8;
-    document.writeln(x+y);
-    console.log(x+y);
+    document.writeln(x + y);
+    console.log(x + y);
 }
 
-function newerFunction(){
-    document.writeln(x+y);
-    console.log(x+y);
+function newerFunction() {
+    document.writeln(x + y);
+    console.log(x + y);
 }
 
-function Morning(){
-    if (new Date().getHours() >= 6){
+function Morning() {
+    if (new Date().getHours() >= 6) {
         document.getElementById("click").innerHTML = "Good Morning";
     } else {
         document.getElementById("click").innerHTML = "Go back to sleep.";
     }
 }
 
-function Hello(){
-    if (new Date().getMonth() == "1"){
+function Hello() {
+    if (new Date().getMonth() == "1") {
         document.getElementById("something").innerHTML = "Happy Birth month!";
     } else {
         document.getElementById("something").innerHTML = "SSDD"
+    }
+}
+
+function Game() {
+    var guess, winningNumber;
+    guess = document.getElementById("number").value;
+    winningNumber = Math.round((Math.random() * 10) + 1);
+    console.log(winningNumber);
+    if (guess == winningNumber) {
+        document.getElementById("results").innerHTML = "You win!";
+    } else if (guess >= 1 && guess <= 10 && guess != winningNumber) {
+        document.getElementById("results").innerHTML = "Sike! That's the wrong number!";
+    } else {
+        document.getElementById("results").innerHTML = "Please enter a number between 1 and 10.";
     }
 }

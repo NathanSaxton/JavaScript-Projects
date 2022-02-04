0375830121be725813490c2048cf1b2562adc721
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-02-04 10:24:51 -0700

    Update main.js
    Added comments and the time function.

--- Basic-JavaScript-Projects/Project7_scope_time_function/js/main.js
+++ Basic-JavaScript-Projects/Project7_scope_time_function/js/main.js
@@ -1,17 +1,17 @@
-var x = 3;
+var x = 3; //global variable
 
-function newFunction() {
-    var y = 8;
+function newFunction() {//add local and global variable
+    var y = 8; //local variable
     document.writeln(x + y);
     console.log(x + y);
 }
 
-function newerFunction() {
+function newerFunction() { //create an error due to lack of local variable
     document.writeln(x + y);
     console.log(x + y);
 }
 
-function Morning() {
+function Morning() { //function using the Date() object
     if (new Date().getHours() >= 6) {
         document.getElementById("click").innerHTML = "Good Morning";
     } else {
@@ -19,7 +19,7 @@ function Morning() {
     }
 }
 
-function Hello() {
+function Hello() { //function using the else statement
     if (new Date().getMonth() == "1") {
         document.getElementById("something").innerHTML = "Happy Birth month!";
     } else {
@@ -27,7 +27,7 @@ function Hello() {
     }
 }
 
-function Game() {
+function Game() { //guessing game i put together using if else statements
     var guess, winningNumber;
     guess = document.getElementById("number").value;
     winningNumber = Math.round((Math.random() * 10) + 1);
@@ -39,4 +39,17 @@ function Game() {
     } else {
         document.getElementById("results").innerHTML = "Please enter a number between 1 and 10.";
     }
+}
+
+function Time_function() { //Time_function() outlined in the slides
+    var Time = new Date().getHours();
+    var Reply;
+    if (Time < 12 == Time > 0) {
+        Reply = "It is morning time.";
+    } else if (Time >= 12 == Time < 18) {
+        Reply = "it is afternoon.";
+    } else {
+        Reply = "it is evening time.";
+    }
+    document.getElementById("Time_of_day").innerHTML = Reply;
 }

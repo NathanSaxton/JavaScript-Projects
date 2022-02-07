eab7586958dc74b895bcd4354d5087d6e463ee4f
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-02-06 22:21:21 -0700

    forLoop added
    Added a for loop to the js and implemented it in the HTML

--- Basic-JavaScript-Projects/Project10_loops_arrays/js/main.js
+++ Basic-JavaScript-Projects/Project10_loops_arrays/js/main.js
@@ -1,12 +1,19 @@
 cont = true;
-tillISay = 5;
+tillISay = document.getElementById("Loop").innerHTML;
 count = 0;
 function callLoop(){
     while (cont == true){
         document.writeln(count);
-        if (count == tillISay) {
+        if (count == tillISay.length) {
             cont = false;
         }
         count++;
     }
+}
+var instruments = ["Goats", "Milk", "Piano", "Pickle"], content = "", j;
+function forLoop(){
+    for (let j = 0; j < instruments.length; j++) {
+        content += instruments[j] + "<br />";
+    }
+    document.getElementById("Instruments").innerHTML = content;
 }

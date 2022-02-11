37a61522f28285f1ad6282fd1ba6f5ed68b9f1c4
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-02-11 15:10:13 -0700

    Update TicTacToe.js
    Set up the .js for the game.

---
+++ Basic-JavaScript-Projects/Ticky-Tacky-Toe/js/TicTacToe.js
@@ -0,0 +1,84 @@
+let activePlayer = 'X';
+let selectedPlayer = [];
+
+function placeXOrO(squareNumber) { //main game function allowing for placing tiles and swapping between player and computer
+    if (!selectedSquares.some(element => element.includes(squareNumber))) {
+        let select = document.getElementById(squareNumber);
+        if (activePlayer === 'X') {//decides what marker is placed
+            select.style.backgroundImage = 'url("../img/x.png")';
+        } else {
+            select.style.backgroundImage = 'url("../img/o.png")';
+        }
+        selectedSquares.push(squareNumber + activePlayer);
+        checkWinConditions();
+        if (activePlayer === 'X') { //change players
+            activePlayer = 'O';
+        } else {
+            activePlayer = 'X';
+        }
+        audio('../media/place.mp3') //placement audio
+        if (activePlayer === 'O') {//make it so the player can't go on the computer's turn
+            disableClick();
+            setTimeout(function () { computersTurn(); }, 1000)
+        }
+        return true;
+    }
+    function computersTurn() { //computer's turn to place on a random square
+        let success = false;
+        let pickASquare;
+        while (!success) {
+            pickASquare = String(Math.floor(Math.random() * 9));
+        }
+        if (placeXOrO(pickASquare)) {
+            placeXOrO(pickASquare);
+            success = true;
+        }
+    }
+}
+
+function checkWinConditions() {
+    if (arrayIncludes('0X', '1X', '2X')) {
+        drawWinLine(50, 100, 558, 100)
+    } else if (arrayIncludes('3X', '4X', '5X')) {
+        drawWinLine(50, 304, 558, 304)
+    } else if (arrayIncludes('6X', '7X', '8X')) {
+        drawWinLine(50, 508, 558, 508)
+    } else if (arrayIncludes('0X', '3X', '6X')) {
+        drawWinLine(100, 50, 100, 558)
+    } else if (arrayIncludes('1X', '4X', '7X')) {
+        drawWinLine(304, 50, 304, 558)
+    } else if (arrayIncludes('2X', '5X', '8X')) {
+        drawWinLine(508, 50, 508, 558)
+    } else if (arrayIncludes('6X', '4X', '2X')) {
+        drawWinLine(100, 508, 510, 90)
+    } else if (arrayIncludes('0X', '4X', '8X')) {
+        drawWinLine(100, 100, 520, 520)
+    } else if (arrayIncludes('0O', '1O', '2O')) {
+        drawWinLine(50, 100, 558, 100)
+    } else if (arrayIncludes('3O', '4O', '5O')) {
+        drawWinLine(50, 304, 558, 304)
+    } else if (arrayIncludes('6O', '7O', '8O')) {
+        drawWinLine(50, 508, 558, 508)
+    } else if (arrayIncludes('0O', '3O', '6O')) {
+        drawWinLine(100, 50, 100, 558)
+    } else if (arrayIncludes('1O', '4O', '7O')) {
+        drawWinLine(304, 50, 304, 558)
+    } else if (arrayIncludes('2O', '5O', '8O')) {
+        drawWinLine(508, 50, 508, 558)
+    } else if (arrayIncludes('6O', '4O', '2O')) {
+        drawWinLine(100, 508, 510, 90)
+    } else if (arrayIncludes('0O', '4O', '8O')) {
+        drawWinLine(100, 100, 520, 520)
+    } else if (selectedSquares.length >= 9) {
+        audio('../media/tie.mp3');
+        setTimeout(function () { resetGame(); }, 1000);
+    }
+
+    arrayIncludes = (squareA, squareB, squareC) => {
+        const a = selectedSquares.includes(squareA)
+        const b = selectedSquares.includes(squareB)
+        const c = selectedSquares.includes(squareC)
+
+        if (a === true && b === true && c === true) { return true }
+    }
+}

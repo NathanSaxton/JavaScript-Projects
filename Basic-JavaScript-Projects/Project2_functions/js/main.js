f85bfd23993ca8e98dd5bdc9d28e98715aba1c51
Nathan Saxton <72226252+NathanSaxton@users.noreply.github.com>
2022-02-18 17:43:47 -0700

    Update main.js
    Added an event listener to create the fade effect.

--- Basic-JavaScript-Projects/Project2_functions/js/main.js
+++ Basic-JavaScript-Projects/Project2_functions/js/main.js
@@ -4,3 +4,7 @@ function newFunction(){ //create the function
     voiceLine = part1 +part2;  //concatenate part1 and part2
     return document.getElementById("line").innerHTML += voiceLine; //concatenate the existing line with the new voice line. 
 }
+
+window.addEventListener("DOMContentLoaded", () => {
+    document.getElementById("line").classList.remove("fade-out");
+});
